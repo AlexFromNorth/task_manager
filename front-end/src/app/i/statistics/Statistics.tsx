03a8d1df0daddbcc899335e5928d2d49666a67ed
AlexFromNorth_desc <jacklex.ab@gmail.com>
2025-04-27 13:58:12 +0300

    feat(statics: change adaptive)

--- front-end/src/app/i/statistics/Statistics.tsx
+++ front-end/src/app/i/statistics/Statistics.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Loader from '@/components/ui/Loader'
+import styles from './Statistics.module.scss'
 
 import { useProfile } from '@/hooks/useProfile'
 import { useTranslation } from 'react-i18next'
@@ -12,7 +13,7 @@ export function Statistics() {
 	return isLoading ? (
 		<Loader />
 	) : (
-		<div className='grid grid-cols-4 gap-12 mt-7'>
+		<div className='grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-12 mt-7'>
 			{data?.statistics.length ? (
 				data.statistics.map(statistic => (
 					<div
